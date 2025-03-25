825faedb37fada2773525d8bcefc155a02a2250f
Yixuan Ding <jiangci456@gmail.com>
2025-03-25 08:23:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,66 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// === Hardware Gpio Temp ===
+
+
+// === 鍑芥暟锛歩nitPWM ===
+void initPWM() {
+    // Set the pins to output mode
+    gpioSetMode(PIN_17, PI_OUTPUT);
+    gpioSetMode(PIN_27, PI_OUTPUT);
+
+    // Set the PWM frequency
+    gpioSetPWMfrequency(PIN_17, PWM_FREQUENCY);
+    gpioSetPWMfrequency(PIN_27, PWM_FREQUENCY);
+
+    // Set the PWM range from 0 to 1000
+    gpioSetPWMrange(PIN_17, 1000);
+    gpioSetPWMrange(PIN_27, 1000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
